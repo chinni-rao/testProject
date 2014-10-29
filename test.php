ea0b0d3bd9e853ad7273957d43e6ff323f59cb37
chinni-rao <kondalarao.chinni@cultuzz.com>
2014-10-29 13:14:17 +0530

    working

--- test.php
+++ test.php
@@ -2,6 +2,6 @@
 	print "tets Git";
 	print "edited in gt";
 	print "test push";
-//	commented for testing multiple projects
+//	commented for testing multiple projects 
 //	edited in the branch code testing again and inserting in master and child branch 
 ?>
